b3e5310d62561a1cc3db786e67fb484bd0492ed5
Prajna1999 <gituprajna20@gmail.com>
2024-09-13 13:17:20 +0530

    ADD:CSV Download for losses and damages

--- src/routes/instagramRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const instagramController = require('../controllers/instagramController');
-const router = express.Router();
-
-router.get('/account', instagramController.getInstagramDetails);
-
-module.exports = router;
--- src/services/drimsApiService.js
+++ src/services/drimsApiService.js
@@ -2,6 +2,10 @@
 const axios = require('axios');
 const config = require('../../config');
 const logger = require('../utils/logger');
+const fs = require('fs');
+const path = require('path');
+// const { generate } = require('csv-generate');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
 class DrimsAPiService {
     constructor() {
@@ -40,13 +44,84 @@ class DrimsAPiService {
                     headers: { 'Authorization': `Bearer ${token}` }
                 }
             );
-            return response.data;
+
+            const result = {
+                affectedPopulation: this.mapData(response.data.affectedPopulation, ['district', 'districtCode', 'totalPopulation', 'totalCropArea']),
+                livesLostConfirmed: this.mapData(response.data.hllDetails?.confirmed, ['district', 'total']),
+                livestockDetails: this.mapData(response.data.livestocksDetails?.affected, ['district', 'total']),
+                houseDamagedDetails: this.mapData(response.data.houseDamagedDetails?.damage, ['district', 'totalHousesFullyDamaged']),
+                emabankmentBreached: this.mapData(response.data.infDamageDetails?.embBreached, ['district', 'total']),
+                embankmentAffected: this.mapData(response.data.infDamageDetails?.embAffected, ['district', 'total']),
+                roadAffected: this.mapData(response.data.infDamageDetails?.roadAffected, ['district', 'total']),
+                bridgeAffected: this.mapData(response.data.infDamageDetails?.bridgeAffected, ['district', 'total'])
+            };
+
+            // generate the csvs
+            await this.generateAndSaveCSVs(result, fromDate, toDate);
+            return {
+                "message": "Succesfully mapped and generated csvs",
+                "result": result
+            };
+
+
         } catch (error) {
             logger.error('Failed to fetch cumulative state flood data', error.message);
             throw error;
         }
     }
 
+    // helper function to map data
+    // generate csv
+    async generateAndSaveCSVs(data, fromDate, toDate) {
+        const csvFolder = path.join('stored-data', 'master_csvs', `${fromDate}_${toDate}`);
+
+        try {
+            // Create the entire directory path if it doesn't exist
+            fs.mkdirSync(csvFolder, { recursive: true });
+            logger.info(`Created directory: ${csvFolder}`);
+        } catch (error) {
+            logger.error(`Error creating directory ${csvFolder}: ${error.message}`);
+            throw error;
+        }
+
+        for (const [key, value] of Object.entries(data)) {
+            if (value.length > 0) {
+                const filename = `${key}_${fromDate}_${toDate}.csv`;
+                const filePath = path.join(csvFolder, filename);
+
+                const csvWriter = createCsvWriter({
+                    path: filePath,
+                    header: Object.keys(value[0]).map(id => ({ id, title: id }))
+                });
+
+                try {
+                    await csvWriter.writeRecords(value);
+                    logger.info(`CSV file generated: ${filename}`);
+                } catch (error) {
+                    logger.error(`Error generating CSV file ${filename}: ${error.message}`);
+                }
+            }
+        }
+    }
+    // map into csv format
+    mapData(data, keys) {
+        return data?.map(item => {
+            const mappedItem = {};
+            keys.forEach(key => {
+                if (key === 'totalPopulation') {
+                    mappedItem[key] = item.total;
+                } else if (key === 'totalCropArea') {
+                    mappedItem[key] = Number(parseInt(item[key]));
+
+                } else if (key === 'totalHousesFullyDamaged') {
+                    mappedItem[key] = item.totalFully
+                } else {
+                    mappedItem[key] = item[key];
+                }
+            });
+            return mappedItem;
+        }) || []
+    }
 
 }
 
