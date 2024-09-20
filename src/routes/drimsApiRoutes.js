9ad993a66602cf72a3b5764a47ca8e0bf950c7f0
Prajna1999 <gituprajna20@gmail.com>
2024-09-20 22:14:30 +0530

    REMOVE: download csv route

--- src/routes/drimsApiRoutes.js
+++ src/routes/drimsApiRoutes.js
@@ -8,5 +8,5 @@ const router = express.Router();
 router.post('/login', drimsApiController.login);
 router.get('/flood/district/consolidated', authHandler, drimsApiController.getStateCumulativeFloodData);
 router.get('/flood/revenue-circle/consolidated', authHandler, drimsApiController.getRevenueCircleData);
-router.get('/flood/download/:filename', authHandler, drimsApiController.downloadRevenueCircleData)
+// router.get('/flood/download/:filename', authHandler, drimsApiController.downloadRevenueCircleData)
 module.exports = router;
